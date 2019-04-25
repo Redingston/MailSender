205712199b868b11df3761d446e7c22b1d306927
Andriy Kondratyuk <kondrat1993yuk@hotmail.com>
2019-04-25 14:35:30 +0300

    Initial

---
+++ MailSender/Models/Email.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Models
+{
+    public class Email : IDataErrorInfo
+    {
+        public string EmailUser { get; set; }
+        public Email()
+        {
+            EmailUser = "";
+        }
+        public string this[string columnName]
+        {
+            get
+            {
+                string errors = "";
+                switch (columnName)
+                {
+                    case "EmailUser":
+                        break;
+                    default:
+                        break;
+                }
+
+                return errors;
+            }
+        }
+
+        public string Error { get; set; }
+    }
+}
---
+++ MailSender/Registration.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MailSender
+{
+    /// <summary>
+    /// Interaction logic for Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ok");
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
